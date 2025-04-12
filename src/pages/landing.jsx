3fec00a8a9f63744ae8755d9a0dc516e99f5f00b
LakshyaPr <prabhakar.lakshya007@gmail.com>
2025-04-13 02:13:24 +0530

    complete version 1

---
+++ src/pages/landing.jsx
@@ -0,0 +1,45 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+
+const LandingPage = () => {
+  const [longUrl, setLongUrl] = useState("");
+  const navigate = useNavigate();
+
+  const handleShorten = (e) => {
+    e.preventDefault();
+    if (longUrl) navigate(`/auth?createNew=${longUrl}`);
+  };
+
+  return (
+    <div className="flex flex-col items-center">
+      <h2 className="my-10 sm:my-16 text-3xl sm:text-6xl lg:text-7xl text-white text-center font-extrabold">
+        The only URL Shortener <br /> you&rsquo;ll ever need! 👇
+      </h2>
+      <form
+        onSubmit={handleShorten}
+        className="sm:h-14 flex flex-col sm:flex-row w-full md:w-2/4 gap-2"
+      >
+        <Input
+          type="url"
+          placeholder="Enter your loooong URL"
+          value={longUrl}
+          onChange={(e) => setLongUrl(e.target.value)}
+          className="h-full flex-1 py-4 px-4"
+        />
+        <Button type="submit" className="h-full" variant="destructive">
+          Shorten!
+        </Button>
+      </form>
+    </div>
+  );
+};
+
+export default LandingPage;
